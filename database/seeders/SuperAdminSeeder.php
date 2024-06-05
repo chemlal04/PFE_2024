3fac4c68787516a952bf046a547003af8556b8e4
MOUAD_CHEMLAL <chemlalmouad2002@gmail.com>
2024-06-05 18:22:46 +0200

    v6

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Models\Team;
 use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Permission;
 
 class SuperAdminSeeder extends Seeder
@@ -18,7 +19,7 @@ class SuperAdminSeeder extends Seeder
     {
         // Check if the super admin user already exists
         $superAdmin = User::firstOrCreate(
-            ['email' => 'superadmin12@example.com'],
+            ['email' => 'superadmin2024@example.com'],
             [
                 'name' => 'Super Admin',
                 'password' => Hash::make('password'),
@@ -42,11 +43,23 @@ class SuperAdminSeeder extends Seeder
 
         // Check if the super-admin role already exists
         $role = Role::firstOrCreate(['name' => 'super-admin']);
+        $allPermissions = Permission::all();
+        foreach ($allPermissions as $permission) {
+            $role->givePermissionTo($permission);
+        }
 
         // Assign the role to the super admin if not already assigned
-        if (!$superAdmin->hasRole($role)) {
-            $superAdmin->assignRole($role);
-        }
+         
+
+        // Custom role assignment to include team_id
+        DB::table('model_has_roles')->updateOrInsert([
+            'role_id' => $role->id,
+            'model_type' => User::class,
+            'model_id' => $superAdmin->id,
+            'team_id' => $team->id, // Explicitly set the team_id
+        ], [
+            'team_id' => $team->id, // Ensure team_id is set
+        ]);
 
         // Optionally, add specific permissions to the role if needed
         // Example: $role->givePermissionTo('some-permission');
